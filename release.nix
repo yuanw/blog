c96b381f73c29ad036f9952b9b30df5e5ef82105
Yuan Wang <me@yuanwang.ca>
2020-01-29 20:47:03 -0600

    wire nix

---
+++ release.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
+
+let
+  inherit (nixpkgs) pkgs;
+  f = import ./default.nix; 
+  haskellPackages = if compiler == "default"
+                    then pkgs.haskellPackages
+                    else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+in
+  drv
---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ nixpkgs ? import <nixpkgs> {} , compiler ? "ghc865" }:
+let
+  inherit (nixpkgs) haskellPackages;
+  myPackages = haskellPackages.callCabal2nix "project" ./blog.cabal  {};
+
+  bootstrap = import <nixpkgs> { };
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    rev = "7f35ed9df40f12a79a242e6ea79b8a472cf74d42";
+    sha256 = "1wr6dzy99rfx8s399zjjjcffppsbarxl2960wgb0xjzr7v65pikz";
+  };
+
+  pinnedPkgs = import src { };
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+in
+haskellPackages.shellFor {
+  withHoogle = true;
+  packages = p: [myPackages];
+  buildInputs = with nixpkgs.haskellPackages;
+    [ hlint
+      ghcid
+      cabal2nix
+      stylish-haskell
+      (all-hies.selection {selector = p: {inherit (p) ghc865; };})
+    ] ++ [pinnedPkgs.cabal-install];
+}
