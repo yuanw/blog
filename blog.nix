c4e9fef29aada357a5eadbd943c06addfe444c4e
Yuan Wang <me@yuanwang.ca>
2020-03-07 20:39:44 -0600

    add base-compact

--- blog.nix
+++ blog.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hakyll, pandoc, stdenv }:
+{ mkDerivation, base, base-compat, hakyll, pandoc, stdenv }:
 mkDerivation {
   pname = "blog";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hakyll pandoc ];
+  executableHaskellDepends = [ base base-compat hakyll pandoc ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
