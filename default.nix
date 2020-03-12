94cb65e364c53de0cae6a6fa3643a0562920fe7f
Yuan Wang <me@yuanwang.ca>
2020-03-11 21:58:17 -0600

    pin down nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
 
 let
-  inherit (nixpkgs) pkgs;
-  f = import ./blog.nix;
-  haskellPackages = if compiler == "default"
-                    then pkgs.haskellPackages
-                    else pkgs.haskell.packages.${compiler};
+  bootstrap = import <nixpkgs> { };
 
-  drv = haskellPackages.callPackage f {};
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src { };
+
 in
-  drv
+  pkgs.haskellPackages.callPackage ./blog.nix {};
