ccee0fdcca99322a61e7de46b0c34e25d8708a99
Yuan Wang <me@yuanwang.ca>
2020-09-11 22:17:18 -0600

    using nix.dev template

---
+++ nix/default.nix
@@ -0,0 +1,43 @@
+{ sources ? import ./sources.nix
+}:
+
+let
+  # default nixpkgs
+  pkgs = import sources.nixpkgs {};
+
+  # gitignore.nix
+  gitignoreSource = (import sources."gitignore.nix" { inherit (pkgs) lib; }).gitignoreSource;
+
+  src = gitignoreSource ./..;
+in
+{
+  inherit pkgs src;
+
+  # provided by shell.nix
+  devTools = {
+    inherit (pkgs)
+      niv
+      pre-commit
+      nodejs
+      hlint
+      cabal2nix
+      cabal-install
+      ;
+    #inherit (pkgs.haskellPackages) wai-app-static;
+    haskell-language-server = pkgs.haskellPackages.haskell-language-server;
+  };
+
+  # to be built by github actions
+  ci = {
+    pre-commit-check = (import sources."pre-commit-hooks.nix").run {
+      inherit src;
+      hooks = {
+        shellcheck.enable = true;
+        nixpkgs-fmt.enable = true;
+        nix-linter.enable = true;
+      };
+      # generated files
+      excludes = [ "^nix/sources\.nix$" ];
+    };
+  };
+}
--- shell.nix
+++ shell.nix
@@ -1,36 +1,9 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc882" }:
-let
-  bootstrap = import <nixpkgs> {};
+{ project ? import ./nix {}
+}:
 
-  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-
-  src = bootstrap.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    inherit (nixpkgs) rev sha256;
-  };
-
-  pkgs = import src {};
-  myHaskellPackages = pkgs.haskellPackages;
-
-  myPackages = myHaskellPackages.callCabal2nix "project" ./blog.cabal {};
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-in
-myHaskellPackages.shellFor {
-  withHoogle = true;
-  packages = p: [ myPackages ];
-  inherit ((import ./pre-commit.nix).pre-commit-check) shellHook;
-  buildInputs = with myHaskellPackages;
-    [
-      hlint
-      ghcid
-      cabal2nix
-      ormolu
-      cabal-install
-      cabal-fmt
-      wai-app-static
-      (all-hies.selection { selector = p: { inherit (p) ghc882; }; })
-    ] ++ [
-      bootstrap.nodejs
-    ];
+project.pkgs.mkShell {
+  buildInputs = builtins.attrValues project.devTools;
+  shellHook = ''
+    ${project.ci.pre-commit-check.shellHook}
+  '';
 }
