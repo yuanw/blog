d3f591e982acc3a00368b513e839289735bfe74c
Yuan Wang <me@yuanwang.ca>
2020-09-21 20:12:20 -0600

    try to speed up the build

--- nix/default.nix
+++ nix/default.nix
@@ -9,42 +9,54 @@ let
   gitignoreSource = (import sources."gitignore.nix" { inherit (pkgs) lib; }).gitignoreSource;
 
   src = gitignoreSource ./..;
+
   blog = pkgs.haskellPackages.callPackage ../blog.nix {};
+
   haskell-env = pkgs.haskellPackages.ghcWithHoogle (
-    hp: with hp; [ cabal-install ]
-    ++ blog.buildInputs
+    hp: with hp; [ cabal-install ] ++ blog.buildInputs
   );
+
+  pre-commit-check = (import sources."pre-commit-hooks.nix").run {
+    inherit src;
+    hooks = {
+      shellcheck.enable = true;
+      nixpkgs-fmt.enable = true;
+      nix-linter.enable = false;
+      cabal-fmt.enable = true;
+      ormolu.enable = true;
+    };
+    # generated files
+    excludes = [ "^nix/sources\.nix$" ];
+  };
+
 in
 {
   inherit pkgs src;
+  # to be built by github actions
+  ci = {
+    blog = blog;
+  };
 
-  # provided by shell.nix
-  devTools = {
-    inherit (pkgs)
+  shell = pkgs.mkShell {
+    name = "blog-env";
+    buildInputs = with pkgs; [
       niv
       pre-commit
       nodejs
       hlint
       yarn
       yarn2nix
-      ;
-  };
+      haskell-env
+    ];
 
-
-  # to be built by github actions
-  ci = {
-    pre-commit-check = (import sources."pre-commit-hooks.nix").run {
-      inherit src;
-      hooks = {
-        shellcheck.enable = true;
-        nixpkgs-fmt.enable = true;
-        nix-linter.enable = false;
-        cabal-fmt.enable = true;
-        ormolu.enable = true;
-      };
-      # generated files
-      excludes = [ "^nix/sources\.nix$" ];
-    };
-    blog = blog;
+    shellHook = ''
+      ${pre-commit-check.shellHook}
+      export HAKYLL_ENV="development"
+      export HIE_HOOGLE_DATABASE="${haskell-env}/share/doc/hoogle/default.hoo"
+      export NIX_GHC="${haskell-env}/bin/ghc"
+      export NIX_GHCPKG="${haskell-env}/bin/ghc-pkg"
+      export NIX_GHC_DOCDIR="${haskell-env}/share/doc/ghc/html"
+      export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+    '';
   };
 }
