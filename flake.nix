21ad848cfeefa2d859ae4cdf0ca3c67f226a8974
Yuan Wang <me@yuanwang.ca>
2021-02-12 20:35:49 -0600

    trying out flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { devShell = import ./shell.nix { inherit pkgs; }; });
+}
