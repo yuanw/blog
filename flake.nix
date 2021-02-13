668ef73ba84b00cea31039e740fee2257443290f
Yuan Wang <me@yuanwang.ca>
2021-02-12 20:41:56 -0600

    trying out flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,39 @@
     flake-utils.url = "github:numtide/flake-utils/master";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in { devShell = import ./shell.nix { inherit pkgs; }; });
+    let
+      overlay = self: super: {
+        haskellPackages = super.haskellPackages.override {
+          overrides = hself: hsuper: {
+            blog = hself.callCabal2nix "blog"
+              (self.nix-gitignore.gitignoreSourcePure [
+                ./.gitignore
+
+              ] ./src) { };
+          };
+        };
+        blog = self.haskell.lib.justStaticExecutables self.haskellPackages.blog;
+      };
+    in {
+      inherit overlay;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in {
+        defaultPackage = pkgs.nix-tree;
+        devShell = pkgs.haskellPackages.shellFor {
+          packages = p: [ p."blog" ];
+          buildInputs = with pkgs.haskellPackages; [
+            cabal-install
+            ghcid
+            ormolu
+            hlint
+            pkgs.nixpkgs-fmt
+          ];
+          withHoogle = false;
+        };
+      });
 }
