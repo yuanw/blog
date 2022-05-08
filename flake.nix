4fbdf6871b553785722236def18841e9fb15cb91
yuanwang <yuan.wang@workiva.com>
2022-05-08 07:47:24 -0600

    update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils/master";
     easy-ps = {
-      url = "github:justinwoo/easy-purescript-nix/master";
+      url = "github:justinwoo/easy-purescript-nix/0f5c141df3ea876b94739b655018168450280587";
       flake = false;
     };
     devshell.url = "github:numtide/devshell/master";
