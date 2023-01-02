52a403a2b6346e0829c3641b0af61e27b00c107d
yuanw <me@yuanwang.ca>
2023-01-01 18:58:24 -0600

    this seems build

--- flake.nix
+++ flake.nix
@@ -3,163 +3,92 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils/master";
-    easy-ps = {
-      url = "github:justinwoo/easy-purescript-nix";
-      flake = false;
-    };
-    devshell.url = "github:numtide/devshell/master";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
   };
-  outputs = { self, nixpkgs, flake-utils, easy-ps, devshell }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = final: prev: {
-          haskellPackages = prev.haskellPackages.override {
-            overrides = hself: hsuper: {
-              blog = (hself.callCabal2nix "blog"
-                (final.nix-gitignore.gitignoreSourcePure [ ./.gitignore ] ./src)
-                { });
-            };
-          };
-          blog =
-            final.haskell.lib.justStaticExecutables final.haskellPackages.blog;
-
-          purs = (final.callPackage easy-ps { }).purs;
-          spago = (final.callPackage easy-ps { }).spago;
-          spago2nix = (final.callPackage easy-ps { }).spago2nix;
-        };
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlay overlay ];
-        };
-        myHaskellEnv = (pkgs.haskellPackages.ghcWithHoogle (p:
-          with p;
-          [ blog cabal-install ormolu hlint hpack brittany warp ]
-          ++ pkgs.blog.buildInputs));
-
-        frontendJs = (import ./purescript { inherit pkgs; }).frontendJs;
-        mkBlogContent = { includeDraft ? false }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      # systems = [ "x86_64-linux" ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+        inputs.pre-commit-hooks-nix.flakeModule
+         inputs.flake-parts.flakeModules.easyOverlay
+      ];
+      perSystem = { self', lib, config, pkgs, ... }: let   mkBlogContent = { includeDraft ? false }:
           pkgs.stdenv.mkDerivation {
             pname =
               if includeDraft then "blog-content-preview" else "blog-content";
-            version = "0.0.3";
+            version = "0.0.4";
             buildInputs = [ pkgs.glibcLocales ];
             LANG = "en_US.UTF-8";
             PREVIEW = if includeDraft then "TRUE" else "FALSE";
             src = ./.;
             buildPhase = ''
-              ${pkgs.blog}/bin/blog rebuild
-              cp ${frontendJs}/frontend.js dist/js/frontend.js
+              ${self'.packages.main-blog}/bin/blog rebuild
               mkdir $out
             '';
             installPhase = ''
               mv dist/* $out
             '';
+          };  in {
+
+            packages.blogContent = mkBlogContent {};
+            packages.draftContent = mkBlogContent {includeDraft = true;};
+
+     # blogContent = mkBlogContent { };
+     #    draftContent = mkBlogContent { includeDraft = true; };
+    # };
+               haskellProjects.main = {
+          packages = {
+            # You can add more than one local package here.
+            blog.root = ./src; # Assumes ./my-package.cabal
           };
-        blogContent = mkBlogContent { };
-        draftContent = mkBlogContent { includeDraft = true; };
-      in {
-        defaultPackage = blogContent;
-        packages = flake-utils.lib.flattenTree {
-          blogContent = blogContent;
-          frontendJs = frontendJs;
+          hlsCheck.enable = false;
+          hlintCheck.enable = true;
         };
-        apps.blog = flake-utils.lib.mkApp { drv = pkgs.blog; };
-        devShell = pkgs.devshell.mkShell {
-          name = "blog-dev-shell";
-          imports = [ (pkgs.devshell.extraModulesDir + "/git/hooks.nix") ];
-          git.hooks.enable = true;
-          git.hooks.pre-commit.text = "${pkgs.treefmt}/bin/treefmt";
-          bash = {
-            extra = ''
-              export LD_INCLUDE_PATH="$DEVSHELL_DIR/include"
-              export LD_LIB_PATH="$DEVSHELL_DIR/lib"
+        pre-commit.settings.hooks = {
+          nixpkgs-fmt.enable = true;
+          cabal-fmt.enable = true;
+          fourmolu.enable = true;
+        };
+        mission-control.scripts = {
+          docs = {
+            description = "Start Hoogle server for project dependencies";
+            exec = ''
+              echo http://127.0.0.1:8888
+              hoogle serve -p 8888 --local
             '';
-            interactive = "";
+            category = "Dev Tools";
           };
-          commands = [
-            {
-              name = "cssWatch";
-              category = "css";
-              command =
-                "ls tailwind/*.css | ${pkgs.entr}/bin/entr ${pkgs.yarn}/bin/yarn build";
-            }
-            {
-              name = "preview";
-              category = "static site";
-              help = "preview static site files";
-              command = "warp -d ${draftContent}";
-            }
-            {
-              name = "siteClean";
-              category = "static site";
-              help = "clean static site files";
-              command = "${pkgs.blog}/bin/blog clean";
-            }
-            {
-              name = "siteWatch";
-              category = "static site";
-              help = "Watch static site files";
-              command = "${pkgs.blog}/bin/blog watch";
-            }
-            {
-              name = "yarn";
-              category = "javascript";
-              package = "yarn";
-            }
-            {
-              name = "node";
-              category = "javascript";
-              package = "nodejs";
-            }
-            {
-              name = "spago";
-              category = "purescript";
-              package = "spago";
-            }
-            {
-              name = "spago2nix";
-              category = "purescript";
-              package = "spago2nix";
-            }
-            {
-              name = "purs";
-              category = "purescript";
-              package = "purs";
-            }
-            {
-              name = "entr";
-              category = "utility";
-              package = "entr";
-            }
-            {
-              name = "buildJs";
-              category = "purescript";
-              help = "Build js from purescript";
-              command =
-                "spago bundle-app -m Frontend -t content/js/frontend.js";
-            }
-          ];
-          # https://github.com/numtide/devshell/blob/master/modules/env.nix#L57
-          env = [
-            {
-              name = "NODE_ENV";
-              value = "development";
-            }
-            {
-              name = "HIE_HOOGLE_DATABASE";
-              value = "${myHaskellEnv}/share/doc/hoogle/default.hoo";
-            }
-            {
-              name = "NIX_GHC";
-              value = "${myHaskellEnv}/bin/ghc";
-            }
-            {
-              name = "NIX_GHCPKG";
-              value = "${myHaskellEnv}/bin/ghc-pkg";
-            }
-          ];
-          packages = [ pkgs.esbuild myHaskellEnv pkgs.nixfmt pkgs.treefmt ];
+          repl = {
+            description = "Start the cabal repl";
+            exec = ''
+              cabal repl "$@"
+            '';
+            category = "Dev Tools";
+          };
+          preview = {
+            exec = "warp -d ${config.packages.draftContent}";
+          };
         };
-      });
+        devShells.default = config.mission-control.installToDevShell (pkgs.mkShell
+          {
+            nativeBuildInputs = [
+              pkgs.nixpkgs-fmt
+              pkgs.pre-commit
+            ];
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          } // self'.devShells.main);
+        # packages.default = self'.packages.main-blog;
+        packages.default = config.packages.blogContent;
+      };
+    };
 }
