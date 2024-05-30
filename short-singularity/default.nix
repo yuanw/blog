2b8b9ad9eb18280222612214fa6318650ecba346
yuanwang <yuan.wang@workiva.com>
2024-05-30 11:40:25 -0600

    how is this

--- short-singularity/default.nix
+++ short-singularity/default.nix
@@ -2,7 +2,7 @@
 
 {pkgs ? import <nixpkgs> {
     inherit system;
-  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_14"}:
+  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_21"}:
 
 let
   nodeEnv = import ./node-env.nix {
