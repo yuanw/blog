9b74267321232839521b67d45ae26fc3b616f6af
yuanwang <yuan.wang@workiva.com>
2024-06-01 06:31:23 -0600

    wip

--- short-singularity/astro.config.mjs
+++ short-singularity/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import org from 'astro-org'
-
+import org from 'astro-org';
 import sitemap from '@astrojs/sitemap';
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [org(), mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [org(), mdx(), sitemap(), starlight()]
 });
