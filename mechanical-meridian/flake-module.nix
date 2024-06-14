844574b23bcde2eeef9c1f8abe19154a0975aeb3
yuanwang <yuan.wang@workiva.com>
2024-06-14 06:59:23 -0600

    update npmDepHash

--- mechanical-meridian/flake-module.nix
+++ mechanical-meridian/flake-module.nix
@@ -23,9 +23,8 @@
         cp -pr --reflink=auto dist $out/
         runHook postInstall
       '';
-      npmDepsHash = lib.fakeHash;
-      # npmDepsHash = "sha256-xvDnu81wK10uGVA9VI98WaKAQ8o9ou+FVBJo5r5bClE=";
-
+      # npmDepsHash = lib.fakeHash;
+      npmDepsHash = "sha256-Iovqpxpv88zCrgMXfsOA7SPUg3gBh/1kq3WQjidXxTs=";
     };
   };
 }
