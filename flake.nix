74bfd3fdca062a671ae66b2f97704c59ba36a410
yuanwang <yuan.wang@workiva.com>
2024-06-14 12:23:56 -0600

    how is this

--- flake.nix
+++ flake.nix
@@ -4,21 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    dream2nix_legacy.url = "github:nix-community/dream2nix";
+    dream2nix.url = "github:nix-community/dream2nix";
     flake-root.url = "github:srid/flake-root";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     devenv.url = "github:cachix/devenv";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts,dream2nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       imports = [
         inputs.flake-root.flakeModule
         inputs.treefmt-nix.flakeModule
         inputs.devenv.flakeModule
-        
-        inputs.dream2nix_legacy.flake-parts
         ./mechanical-meridian/flake-module.nix
       ];
       perSystem = { self', lib, config, pkgs, ... }:
