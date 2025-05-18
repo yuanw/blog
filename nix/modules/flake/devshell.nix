a71a5b70c6423202fcba9aa7d1fc1243de617b47
yuanw <me@yuanwang.ca>
2025-05-18 06:22:57 -0600

    addq

--- nix/modules/flake/devshell.nix
+++ nix/modules/flake/devshell.nix
@@ -1,21 +1,38 @@
 {
-  perSystem = { config, pkgs, ... }: {
+  perSystem = { config, pkgs, lib, ... }:
     # Default shell.
-    devShells.default = pkgs.mkShell {
-      name = "blog-dev-shell";
-      meta.description = "Haskell development environment";
-      # See https://community.flake.parts/haskell-flake/devshell#composing-devshells
-      inputsFrom = [
-        config.haskellProjects.default.outputs.devShell # See ./nix/modules/haskell.nix
-        config.pre-commit.devShell # See ./nix/modules/formatter.nix
-      ];
-      packages = with pkgs; [
-        just
-        nixd
-        ghciwatch
-        haskellPackages.wai-app-static
-        haskellPackages.hakyll
-      ];
+    let
+      shiki = pkgs.buildNpmPackage rec {
+        pname = "shiki";
+        version = "3.4.2";
+
+        src = lib.fetchFromGitHub {
+          owner = "shikijs";
+          repo = "shiki";
+          rev = "v${version}";
+          hash = "sha256-J7H1oofgosFGxoHzcx+UxaRbqGwqrmk6MYmMISpNB6w=";
+        };
+
+        npmDepsHash = "sha256-/YWsk+GNfudSG0Rof1eCXeoK6dfyzzQqvWBLkpfahE0=";
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        name = "blog-dev-shell";
+        meta.description = "Haskell development environment";
+        # See https://community.flake.parts/haskell-flake/devshell#composing-devshells
+        inputsFrom = [
+          config.haskellProjects.default.outputs.devShell # See ./nix/modules/haskell.nix
+          config.pre-commit.devShell # See ./nix/modules/formatter.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd
+          shiki
+          ghciwatch
+          haskellPackages.wai-app-static
+          haskellPackages.hakyll
+        ];
+      };
     };
-  };
 }
